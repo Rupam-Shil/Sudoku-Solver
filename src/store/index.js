66bb76f7442c4d9aac7afd1677c9834ca61d5c10
Rupam Shil <rupamshil111@gmail.com>
2022-06-20 21:15:48 +0530

    new pages added

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import create from "zustand";
+import { devtools, persist } from "zustand/middleware";
+import { sudokuSolver } from "../utils/helpers";
+
+const createSudoku = (set) => ({
+	rawSudokuArr: [
+		[3, 0, 6, 5, 0, 8, 4, 0, 0],
+		[5, 2, 0, 0, 0, 0, 0, 0, 0],
+		[0, 8, 7, 0, 0, 0, 0, 3, 1],
+		[0, 0, 3, 0, 1, 0, 0, 8, 0],
+		[9, 0, 0, 8, 6, 3, 0, 0, 5],
+		[0, 5, 0, 0, 9, 0, 6, 0, 0],
+		[1, 3, 0, 0, 0, 0, 2, 5, 0],
+		[0, 0, 0, 0, 0, 0, 0, 7, 4],
+		[0, 0, 5, 2, 0, 6, 3, 0, 0],
+	],
+	solvedSudokuArr: [
+		[3, 0, 6, 5, 0, 8, 4, 0, 0],
+		[5, 2, 0, 0, 0, 0, 0, 0, 0],
+		[0, 8, 7, 0, 0, 0, 0, 3, 1],
+		[0, 0, 3, 0, 1, 0, 0, 8, 0],
+		[9, 0, 0, 8, 6, 3, 0, 0, 5],
+		[0, 5, 0, 0, 9, 0, 6, 0, 0],
+		[1, 3, 0, 0, 0, 0, 2, 5, 0],
+		[0, 0, 0, 0, 0, 0, 0, 7, 4],
+		[0, 0, 5, 2, 0, 6, 3, 0, 0],
+	],
+	solveSudoku: () =>
+		set((state) => ({ solvedSudokuArr: sudokuSolver(state.rawSudokuArr) })),
+});
+const useSudoku = create(
+	devtools(
+		persist(createSudoku, {
+			name: "sudoku",
+		})
+	)
+);
+
+export default useSudoku;
